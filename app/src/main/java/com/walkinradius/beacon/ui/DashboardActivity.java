93006215e045650f184a3e29f3d4c576a2c847f7
vikas <vikaspatel7783@gmail.com>
2018-07-30 22:50:02 +0530

    dashboard activity action bar

--- app/src/main/java/com/walkinradius/beacon/ui/DashboardActivity.java
+++ app/src/main/java/com/walkinradius/beacon/ui/DashboardActivity.java
@@ -1,8 +1,8 @@
 package com.walkinradius.beacon.ui;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -19,7 +19,7 @@ import com.walkinradius.beacon.presenter.DashboardViewPresenter;
 
 import java.util.List;
 
-public class DashboardActivity extends Activity implements DashboardViewContract.DashboardView {
+public class DashboardActivity extends AppCompatActivity implements DashboardViewContract.DashboardView {
 
     private ProgressBar pgBarLogin;
     private RecyclerView mRecyclerView;
