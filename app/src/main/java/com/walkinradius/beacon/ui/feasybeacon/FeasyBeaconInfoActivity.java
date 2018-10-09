bdde801ab01e171ab131da8601c4e0d763c374e6
vikas <vikaspatel7783@gmail.com>
2018-10-09 22:41:50 +0530

    progress dialog shown while connecting beacon

--- app/src/main/java/com/walkinradius/beacon/ui/feasybeacon/FeasyBeaconInfoActivity.java
+++ app/src/main/java/com/walkinradius/beacon/ui/feasybeacon/FeasyBeaconInfoActivity.java
@@ -3,9 +3,12 @@ package com.walkinradius.beacon.ui.feasybeacon;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.View;
+import android.widget.TextView;
 
 import com.feasycom.bean.BluetoothDeviceWrapper;
 import com.feasycom.bean.FeasyBeacon;
+import com.walkinradius.beacon.R;
 import com.walkinradius.beacon.feasybeacon.FeasyBeaconSdkCallback;
 import com.walkinradius.beacon.feasybeacon.FeasyBeaconSdkFacade;
 import com.walkinradius.beacon.ui.ParentActivity;
@@ -23,6 +26,8 @@ public class FeasyBeaconInfoActivity extends ParentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_feasy_beacon_info);
+
         mSelectedBeacon = (BluetoothDeviceWrapper) getIntent().getSerializableExtra(KEY_SELECTED_BEACON);
 
         if (null != mSelectedBeacon.getFeasyBeacon() &&
@@ -31,6 +36,9 @@ public class FeasyBeaconInfoActivity extends ParentActivity {
             FeasyBeaconPinDialog feasyBeaconPinDialog = new FeasyBeaconPinDialog(this);
             feasyBeaconPinDialog.show();
         }
+
+        mFeasyBeaconSdkFacade = FeasyBeaconSdkFacade.getInstance();
+        mFeasyBeaconSdkFacade.setSdkCallback(new SdkCallback());
     }
 
     public void onPinEntered(String pin) {
@@ -40,8 +48,24 @@ public class FeasyBeaconInfoActivity extends ParentActivity {
             return;
         }
         mPin = pin;
-        mFeasyBeaconSdkFacade = FeasyBeaconSdkFacade.getInstance();
-        mFeasyBeaconSdkFacade.setSdkCallback(new SdkCallback());
+
+        showProgressbar(R.string.label_beacon_connect_default);
+    }
+
+    private void showProgressbar(int messageId) {
+        setProgressLayoutVisibility(true);
+
+        TextView textViewPgBar = findViewById(R.id.textView_Pgbar_BeaconInfo);
+        textViewPgBar.setText(getString(messageId));
+    }
+
+    private void hidProgressbar() {
+        setProgressLayoutVisibility(false);
+    }
+
+    private void setProgressLayoutVisibility(boolean visible) {
+        View layoutProgressbar = findViewById(R.id.layout_beaconinfo_pgbar);
+        layoutProgressbar.setVisibility(visible ? View.VISIBLE : View.GONE);
     }
 
     public void showMessageAndFinishActivity(String message) {
